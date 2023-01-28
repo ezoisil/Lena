b56b352c115226c22c37a90cb081edfde72b9250
ezoisil <e.isilozcelik@gmail.com>
2023-01-28 12:36:39 +0300

    Added scriptable variables and playerHP

---
+++ Lena/Assets/_Scripts/_ScriptableVariables/FloatVariable.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace _Scripts._ScriptableVariables
+{
+
+    [CreateAssetMenu(menuName = "Float Variable")]
+    public class FloatVariable : ScriptableObject
+    {
+        public float Value;
+    }
+
+}
