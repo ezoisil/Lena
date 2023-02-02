6e71a5fd71e9adafa02a60cf5700356443c8f7b2
ezoisil <e.isilozcelik@gmail.com>
2023-02-02 16:29:37 +0300

    Implemented player movement

--- Lena/Assets/Scripts/Player/PlayerMovement.cs
+++ Lena/Assets/Scripts/Player/PlayerMovement.cs
@@ -9,10 +9,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private InputReaderSO _inputReader;
     [SerializeField] private Rigidbody _rigidbody;
 
-    [NonSerialized] public Vector3 MovementInput;
     [NonSerialized] public Vector3 MovementVector;
     [NonSerialized] public bool DashInput;
-    [NonSerialized] public bool SprintInput;
+    [NonSerialized] public bool IsSprinting;
+
+    private int _slowDownBuffer = 0;
+    private Vector3 _movementInput;
+    private bool _isMoving;
 
     #region Movement Vector Calculation Specific Variables
 
@@ -27,17 +30,21 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
-        _mainCameraTransform = Camera.main.transform;
+        if (Camera.main != null)
+            _mainCameraTransform = Camera.main.transform;
     }
 
     private void Update()
     {
         CalculateMovementVector();
+        if(_isMoving)
+            Move();
     }
 
     private void OnEnable()
     {
         _inputReader.MoveEvent += OnMove;
+        _inputReader.DashEvent += OnDash;
         _inputReader.EnableGameInput();
     }
 
@@ -51,17 +58,16 @@ public class PlayerMovement : MonoBehaviour
     private void OnMove(Vector2 dislocation)
     {
         Debug.Log(dislocation);
-        MovementInput = dislocation;
+        _movementInput = dislocation;
         _rigidbody.velocity = dislocation;
     }
 
     private void OnDash()
     {
-        
+        DashInput = true;
     }
 
     #endregion
-
     private void CalculateMovementVector()
     {
         _cameraForward = _mainCameraTransform.forward;
@@ -70,11 +76,33 @@ public class PlayerMovement : MonoBehaviour
         _cameraForward.y = 0;
         _cameraRight.y = 0;
 
-        _adjustedMovement = _cameraRight.normalized * MovementInput.x + _cameraForward.normalized * MovementInput.y;
+        _adjustedMovement = _cameraRight.normalized * _movementInput.x + _cameraForward.normalized * _movementInput.y;
 
-        if (MovementInput.sqrMagnitude == 0f)
+        if (_movementInput.sqrMagnitude == 0f)
         {
             _adjustedMovement = transform.forward * (_adjustedMovement.magnitude + .01f);
+            _isMoving = false;
         }
+        
+        _targetSpeed = Mathf.Clamp01(_movementInput.magnitude);
+        if (_targetSpeed > 0f)
+        {
+            _isMoving = true;
+            if (IsSprinting)
+                _targetSpeed = 1f;
+
+            if (_slowDownBuffer > 0)
+                _targetSpeed = .05f;
+        }
+        _targetSpeed = Mathf.Lerp(_previousSpeed, _targetSpeed, Time.deltaTime * 4f);
+
+        MovementVector = _adjustedMovement.normalized * _targetSpeed;
+
+        _previousSpeed = _targetSpeed;
+    }
+
+    private void Move()
+    {
+        _rigidbody.velocity = MovementVector;
     }
 }
