1df04092dcfd0febaa4a91d99591da266cc7ad74
ezoisil <e.isilozcelik@gmail.com>
2023-04-06 13:08:13 +0300

    Added HurtBoxMask to HitBox
    Now CheckHit compares the HurtBoxType and HurtBoxMask

--- Lena URP/Assets/Scripts/Combat System/HitBox.cs
+++ Lena URP/Assets/Scripts/Combat System/HitBox.cs
@@ -7,55 +7,56 @@ namespace Combat_System
     {
         [SerializeField] private BoxCollider _collider;
         [SerializeField] private LayerMask _layerMask;
+        [SerializeField] public HurtBoxMask _hurtBoxMask = HurtBoxMask.Enemy;
 
         [SerializeField] private float _thickness = 0.025f;
         private IHitResponder _hitResponder;
 
-        public IHitResponder HitResponder
-        {
-            get => _hitResponder;
-            set => _hitResponder = value;
-        }
+        public IHitResponder HitResponder { get => _hitResponder; set => _hitResponder = value; }
 
-        private float distance;
-        private Vector3 direction;
-        private Vector3 center;
-        private Vector3 startPoint;
-        private Vector3 halfExtents;
-        private Quaternion orientation;
-        private RaycastHit[] results;
-        private int size;
+        private float _distance;
+        private Vector3 _direction;
+        private Vector3 _center;
+        private Vector3 _startPoint;
+        private Vector3 _halfExtents;
+        private Quaternion _orientation;
+        private RaycastHit[] _results;
+        private Vector3 _lossyScale;
+        private int _size;
+
         public void CheckHit()
         {
             var size1 = _collider.size;
+            _lossyScale = transform.lossyScale;
             Vector3 scaledSize = new Vector3(
-            size1.x * transform.lossyScale.x,
-            size1.y * transform.lossyScale.y,
-            size1.z * transform.lossyScale.z
+            size1.x * _lossyScale.x,
+            size1.y * _lossyScale.y,
+            size1.z * _lossyScale.z
             );
 
-            distance = scaledSize.y - _thickness;
-            direction = transform.up;
-            center = transform.TransformPoint(_collider.center);
-            startPoint = center - direction * (distance / 2);
-            halfExtents = new Vector3(scaledSize.x, _thickness, scaledSize.z) / 2;
-            orientation = transform.rotation;
+            _distance = scaledSize.y - _thickness;
+            _direction = transform.up;
+            _center = transform.TransformPoint(_collider.center);
+            _startPoint = _center - _direction * (_distance / 2);
+            _halfExtents = new Vector3(scaledSize.x, _thickness, scaledSize.z) / 2;
+            _orientation = transform.rotation;
 
-            results = new RaycastHit[10];
-            size = Physics.BoxCastNonAlloc(startPoint, halfExtents, direction, results, orientation, distance, _layerMask);
+            _results = new RaycastHit[10];
+            _size = Physics.BoxCastNonAlloc(_startPoint, _halfExtents, _direction, _results, _orientation, _distance, _layerMask);
 
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < _size; i++)
             {
                 if (TryGetComponent(out IHurtBox hurtBox))
                 {
                     if (!hurtBox.IsActive) return;
+                    if(!_hurtBoxMask.HasFlag(hurtBox.Type)) return;
 
-                    RaycastHit hit = results[i];
+                    RaycastHit hit = _results[i];
 
                     var hitData = new HitData
                     {
                         Damage = _hitResponder?.Damage ?? 0,
-                        HitPoint = hit.point == Vector3.zero ? center : hit.point,
+                        HitPoint = hit.point == Vector3.zero ? _center : hit.point,
                         HitNormal = hit.normal,
                         HurtBox = hurtBox,
                         HitDetector = this
